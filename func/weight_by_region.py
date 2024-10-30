6a515371504f3b98788992062f53e784a41bb196
NongShiN <hcw1027@gmail.com>
2024-10-30 23:19:18 +0900

    feat #15: modularize calculate functions

--- func/weight_by_region.py
+++ func/weight_by_region.py
@@ -5,44 +5,42 @@ import json
  
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 
-from dataloader.dataloader import load_od, load_stay
+from dataloader.dataloader import load_od, load_stay, load_address
 
-# 'data/raw_data' 경로 설정
-with open('config.json', 'r', encoding='utf-8') as f:
-    paths = json.load(f)
+def total_number_of_residents_by_region():
+    df_stay = load_stay()
+    
+    # 지역별 전체 거주민 수
+    df_stay_all = df_stay.groupby(['hdong_cd', 'date'])['stay_cnts'].sum().reset_index()  # 날짜별 거주인구 합산
+    avg_stay_cnts_all = round(df_stay_all.groupby('hdong_cd')['stay_cnts'].mean().reset_index(), 0)  # 하루 평균 거주인구
 
-root_dir = paths['root_dir']
-address = pd.read_csv(os.path.join(root_dir, "address_with_lon_lat_final.csv"))  # 행정동코드 + 위도경도 (행정동코드,시도명,시군구명,읍면동명,동리명,위도,경도)
-address['시도 시군구'] = address['시도명'].fillna('') + ' ' + address['시군구명'].fillna('')
-address['시도 시군구'] = address['시도 시군구'].str.strip() # 양쪽 값이 모두 null인 경우 빈 문자열 처리
+    # 지역별 20대,50대,60대 거주민 수
+    df_stay_age = df_stay[df_stay['age'].isin([2,5,6])]  # 날짜별 20대 거주인구
+    avg_stay_cnts_age = round(df_stay_age.groupby(['hdong_cd'])['stay_cnts'].mean().reset_index(), 0)  # 하루 평균 20대 거주인구
 
+    return avg_stay_cnts_all, avg_stay_cnts_age
 
-####
-df_od = load_od()
-df_stay = load_stay()
+def number_of_visitors_to_Muju_by_region():
+    df_od = load_od()
+    
+    # 지역별 무주축제 방문객 인원수
+    df_od_group = df_od.groupby(['origin_hdong_cd', 'date', 'age'])['od_cnts'].sum().reset_index()
+    df_od_all = df_od_group.groupby(['origin_hdong_cd', 'date'])['od_cnts'].sum().reset_index()  # 날짜별 방문객수 합산
+    sum_od_cnts_all = round(df_od_all.groupby(['origin_hdong_cd'])['od_cnts'].sum().reset_index(), 0)  # 해당 지역에서 온 전체 방문객수
 
-# 지역별 전체 거주민 수
-df_stay_all = df_stay.groupby(['hdong_cd', 'date'])['stay_cnts'].sum().reset_index()  # 날짜별 거주인구 합산
-avg_stay_cnts_all = round(df_stay_all.groupby('hdong_cd')['stay_cnts'].mean().reset_index(), 0)  # 하루 평균 거주인구
+    # 지역별 20대,50대,60대 무주축제방문객 수
+    df_od_age = df_od_group[df_od_group['age'].isin([2,5,6])]
+    sum_od_cnts_age = round(df_od_age.groupby('origin_hdong_cd')['od_cnts'].sum().reset_index(), 0)
 
-# 지역별 20대,50대,60대 거주민 수
-df_stay_age = df_stay[df_stay['age'].isin([2,5,6])]  # 날짜별 20대 거주인구
-avg_stay_cnts_age = round(df_stay_age.groupby(['hdong_cd'])['stay_cnts'].mean().reset_index(), 0)  # 하루 평균 20대 거주인구
-
-# 지역별 무주축제 방문객 인원수
-df_od_group = df_od.groupby(['origin_hdong_cd', 'date', 'age'])['od_cnts'].sum().reset_index()
-df_od_all = df_od_group.groupby(['origin_hdong_cd', 'date'])['od_cnts'].sum().reset_index()  # 날짜별 방문객수 합산
-sum_od_cnts_all = round(df_od_all.groupby(['origin_hdong_cd'])['od_cnts'].sum().reset_index(), 0)  # 해당 지역에서 온 전체 방문객수
-
-# 지역별 20대,50대,60대 무주축제방문객 수
-df_od_age = df_od_group[df_od_group['age'].isin([2,5,6])]
-sum_od_cnts_age = round(df_od_age.groupby('origin_hdong_cd')['od_cnts'].sum().reset_index(), 0)
-
+    return sum_od_cnts_all, sum_od_cnts_age
 
 # 각 지역 거주인원
 def get_residents_num(lst):
+    address = load_address()
+    avg_stay_cnts_all, avg_stay_cnts_age = total_number_of_residents_by_region()
     residents_num = []
     residents_num_256 = []
+
     for name in lst:
         #print("지역명:", name)
         codes = address[address['시도 시군구'] == name]['행정동코드'].unique().tolist()
@@ -64,8 +62,11 @@ def get_residents_num(lst):
 
 # 각 지역 방문객
 def get_visitors_num(lst):
+    address = load_address()
+    sum_od_cnts_all, sum_od_cnts_age = number_of_visitors_to_Muju_by_region()
     visitors_num = []
     visitors_num_256 = []
+
     for name in lst:
         #print("지역명:", name)
         codes = address[address['시도 시군구'] == name]['행정동코드'].unique().tolist()
@@ -114,8 +115,5 @@ def get_weights(nodes, weights):
     
     stations = {nodes: weight for nodes, weight in zip(nodes, softmax_weights)}
     stations['전라북도 무주군'] = 0 # 무주군은 목적지이므로 가중치 0 으로 설정  
-    
-    #print("정류장 가중치")
-    #print(stations)
 
     return stations
